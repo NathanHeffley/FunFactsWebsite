21ee20204637e4306aa1b252fe978a9be38db8d5
Nathan Heffley <contact@nathanheffley.com>
2016-05-23 01:22:38 -0400

    Fun Facts Website and Server

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  request('http://localhost:3000/api/0-0-1/funfact', function(error, response, body) {
+    console.log("Retrieved fun fact: " + body);
+    res.render('index', { title: 'Fun Facts', funfact: body });
+  });
+});
+
+module.exports = router;
