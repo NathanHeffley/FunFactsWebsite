261bdc951420b91bb4db46fc8fba423d4061f085
Nathan Heffley <nathanheffley@gmail.com>
2016-05-28 13:05:58 -0400

    Fix error when docs isn't set

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 var Fact = require('../db').Fact;
 
+var missingFact = {
+  id: "missingfact",
+  text: "I'm sorry, I can't remember that fact:("
+}
+
 // API information routes
 router.get('/', function(req, res, next) {
   res.render('api', { title: 'Fun Facts API' });
@@ -27,18 +32,19 @@ router.get('/v1/fact/:factId', function(req, res, next) {
 
 // Helper functions
 var cleanFact = function(docs) {
-  if (docs[0] != null) {
-    var factData = docs[0];
-    var fact = {
-      id: factData.id,
-      text: factData.text
+  if (docs != null) {
+    if (docs[0] != null) {
+      var factData = docs[0];
+      var fact = {
+        id: factData.id,
+        text: factData.text
+      }
+      return fact;
+    } else {
+      return missingFact;
     }
-    return fact;
   } else {
-    return fact = {
-      id: "missingfact",
-      text: "I'm sorry, I can't remember that fact :("
-    }
+    return missingFact;
   }
 };
 
