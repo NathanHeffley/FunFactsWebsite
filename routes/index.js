413fb04f8b2d247bcccae1cff786c1c121e95e05
Nathan Heffley <nathanheffley@gmail.com>
2016-05-27 23:55:52 -0400

    Add front-end page to display specific facts

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,13 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/:factId', function(req, res, next) {
+  var url = 'http://localhost:3000/api/v1/fact/' + req.params.factId;
+  request(url, function(error, response, body) {
+    console.log("Retrieved fun fact: " + body);
+    var fact = JSON.parse(body);
+    res.render('index', { title: 'Fun Facts', fact: fact });
+  });
+});
+
 module.exports = router;
