386dca9d9f214fd96314a3b49863b06be1580838
Nathan Heffley <nathanheffley@gmail.com>
2016-05-27 23:21:43 -0400

    Add id display to index page

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   request('http://localhost:3000/api/v1/fact/random', function(error, response, body) {
     console.log("Retrieved fun fact: " + body);
     var fact = JSON.parse(body);
-    res.render('index', { title: 'Fun Facts', funfact: fact.text });
+    res.render('index', { title: 'Fun Facts', fact: fact });
   });
 });
 
